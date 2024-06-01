ede5eb4c907633bcaf1f6b20b988b4414c4d688d
Adida <adida.barack@student.moringaschool.com>
2024-06-01 21:59:07 +0300

    Updated structures

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-const nums = ["aee", "oeu", "iio", "aeu", "aei", "ouu"]
+const nums = ["aee", "oeu", "iio", "aeu", "aei", "ouu", "aou"]
 let numbers = nums.join('').split('').sort().join('').replace(/[a]/gi, '')
 
 
